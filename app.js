8ea99b640e1701006f4249b5426cbb48adb522b6
Mikolaj Cymcyk <mic5@hi.is>
2021-05-06 16:13:27 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const videos = require('./src/videos');
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = '*';
 
 const app = express();
 app.use(express.static('public'));
